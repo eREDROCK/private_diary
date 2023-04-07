d3fe3242cf3056668056632a55c5f38e18afe85e
eREDROCK <rdrc136@gmail.com>
2023-04-07 12:41:14 +0900

    [Fix]ulr.pyに関するバクを修正、ページが表示されるようになった

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'static'),
 )
 
 # Quick-start development settings - unsuitable for production
