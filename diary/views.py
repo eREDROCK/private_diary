329e860c7fe3b0d9a25fa72e849e1bd3c0fd507c
eREDROCK <rdrc136@gmail.com>
2023-04-29 20:58:01 +0900

    [Add]日記のページの見た目を作った

--- diary/views.py
+++ diary/views.py
@@ -1,14 +1,14 @@
 import logging
+from typing import Any
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
-
 from django.views import generic
-
 from django.shortcuts import render
-
 from.forms import InquiryForm
-
 from django.contrib import messages
+from .models import Diary
 
 logger=logging.getLogger(__name__)
 
@@ -28,3 +28,11 @@ class InquiryView(generic.FormView):
     messages.success(self.request,'メッセージを送信しました。')
     logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
     return super().form_valid(form)
+
+class DiaryListView(LoginRequiredMixin,generic.ListView):
+  model=Diary
+  template_name='diary_list.html'
+
+  def get_queryset(self):
+    diaries=Diary.objects.filter(user=self.request.user).order_by('-created_at')
+    return diaries
