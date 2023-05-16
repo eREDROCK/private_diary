dbd42566132c742510b08a696c571336c8bdab5c
eREDROCK <rdrc136@gmail.com>
2023-05-16 16:15:00 +0900

    [Add]日記詳細表示ページと日記新規作成ページとその処理を作成

--- diary/views.py
+++ diary/views.py
@@ -3,10 +3,12 @@ from typing import Any
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
-from.forms import InquiryForm
+from.forms import InquiryForm, DiaryCreateForm
 from django.contrib import messages
 from .models import Diary
 
@@ -37,3 +39,24 @@ class DiaryListView(LoginRequiredMixin,generic.ListView):
   def get_queryset(self):
     diaries=Diary.objects.filter(user=self.request.user).order_by('-created_at')
     return diaries
+  
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+  model=Diary
+  template_name='diary_detail.html'
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+  model=Diary
+  template_name='diary_create.html'
+  form_class=DiaryCreateForm
+  success_url=reverse_lazy('diary:diary_list')
+
+  def form_valid(self, form):
+    diary=form.save(commit=False)
+    diary.user=self.request.user
+    diary.save()
+    messages.success(self.request, '日記を作成しました。')
+    return super().form_valid(form)
+  
+  def form_invalid(self, form):
+    messages.error(self.request,"日記の作成に失敗しました。")
+    return super().form_invalid(form)
